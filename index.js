536ec36a9bb87965d915f3f84819f9258adddab7
isaiah2k <166669354+isaiah2k@users.noreply.github.com>
2024-05-24 11:05:15 -0700

    Updates index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+function introduction(name) {
+    return (`Hi, my name is ${name}.`)
+    console.log(introduction(`Isaiah`));
+}
+function introductionWithLanguage(name, language) {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+    console.log(introductionWithLanguage(`Isaiah`), (`JavaScript`))
+}
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+    console.log(introductionWithLanguage(`Isaiah`), (`JavaScript`))
+}
